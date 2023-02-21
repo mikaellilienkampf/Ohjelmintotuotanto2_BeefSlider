7728999f21d325390694a41acb0a4b1206470436
Roope Plattonen <roope.plattonen@edu.karelia.fi>
2023-02-21 18:26:39 +0200

    Muokattu "lisää uusi tuote".

---
+++ android/app/src/main/java/com/bonuscarnisapp/lisaaUusiTuoteActivity.java
@@ -0,0 +1,14 @@
+package com.bonuscarnisapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lisaaUusiTuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lisaa_uusi_tuote);
+    }
+}
