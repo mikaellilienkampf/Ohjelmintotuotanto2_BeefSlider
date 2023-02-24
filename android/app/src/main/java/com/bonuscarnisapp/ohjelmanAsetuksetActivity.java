7a975a07ea32d01736d0962e8efff93c423da675
Mikael Lilienkampf <Mikael@student.uef.fi>
2023-02-24 16:04:08 +0200

    Asetuksiin switchbutton ja AsetuksetActivityyn toiminallisuus. (ei toimi)

--- android/app/src/main/java/com/bonuscarnisapp/ohjelmanAsetuksetActivity.java
+++ android/app/src/main/java/com/bonuscarnisapp/ohjelmanAsetuksetActivity.java
@@ -3,12 +3,20 @@ package com.bonuscarnisapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+
+
 public class ohjelmanAsetuksetActivity extends AppCompatActivity {
+
+    private Switch switchTeema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +30,45 @@ public class ohjelmanAsetuksetActivity extends AppCompatActivity {
         // Päivitetään otsikko
         actionBar.setTitle("Asetukset");
 
+        //Tarkistetaan, onko teema jo asetettu
+        SharedPreferences prefs = getSharedPreferences("THEME", MODE_PRIVATE);
+        int themeId = prefs.getInt("theme", R.style.LightTheme);
+        setTheme(themeId);
+        // Night mode switch
+        switchTeema = findViewById(R.id.switchTeema);
+        //Asettaa switchin tiettyyn tilaan teeman mukaan
+        if (themeId == R.style.LightTheme) {
+            switchTeema.setChecked(false);
+        } else {
+            switchTeema.setChecked(true);
+        }
+        switchTeema.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                int themeId;
+                if(isChecked){
+                    themeId = R.style.DarkTheme;
+                } else {
+                    themeId = R.style.LightTheme;
+                }
+                changeTheme(themeId);
+
+            }
+            private void changeTheme(int themeId) {
+                SharedPreferences.Editor editor = getSharedPreferences("THEME", MODE_PRIVATE).edit();
+                editor.putInt("theme", themeId);
+                editor.apply();
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 
     /*
-    Metodi, joka palauttaa ohjelman päävalikkoon, kun käyttäjä painaa ActionBarissa olevaa nuolta.
-    Ei kysy käyttäjältä erillistä varmistusta.
-     */
+        Metodi, joka palauttaa ohjelman päävalikkoon, kun käyttäjä painaa ActionBarissa olevaa nuolta.
+        Ei kysy käyttäjältä erillistä varmistusta.
+         */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
