225d9ca73f5bee00490b5761ee9665e47e3bef19
Tomi Karjalainen <tomikar@uef.fi>
2023-02-26 21:44:21 +0200

    Käyttäjä voi lisätä/muokata oletussähköpostiosoitetta asetuksissa

--- android/app/src/main/java/com/bonuscarnisapp/ohjelmanAsetuksetActivity.java
+++ android/app/src/main/java/com/bonuscarnisapp/ohjelmanAsetuksetActivity.java
@@ -6,20 +6,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.InputType;
 import android.view.MenuItem;
 
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Switch;
-
+import android.widget.TextView;
 
 
 public class ohjelmanAsetuksetActivity extends AppCompatActivity {
 
+    // tvAsetettuSahkopostiosoite
+    private TextView textViewAsetettuSahkopostiosoite;
+    // ibMuokkaaAsetettuaSahkopostiosoitetta
+    private ImageButton imageButtonMuokkaaAsetettuaSahkopostiosoitetta;
     private Switch switchTeema;
     private SharedPreferences sharedPref;
     private boolean isDarkTheme;
@@ -42,7 +53,7 @@ public class ohjelmanAsetuksetActivity extends AppCompatActivity {
         // ...ActionBarin Paluu-painike näkyviin
         actionBar.setDisplayHomeAsUpEnabled(true);
         // Päivitetään otsikko
-        actionBar.setTitle("Asetukset");
+        actionBar.setTitle("... palaa alkuvalikkoon");
 
         switchTeema = findViewById(R.id.switchTeema);
 
@@ -63,6 +74,54 @@ public class ohjelmanAsetuksetActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Haetaan aiemmin asetettu oletussähköposti tvAsetettuSahkopostiosoite-textViewiin
+        String oletussahkoposti = sharedPref.getString("defaultEmail", null);
+        textViewAsetettuSahkopostiosoite = findViewById(R.id.tvAsetettuSahkopostiosoite);
+        if(oletussahkoposti == null){
+            textViewAsetettuSahkopostiosoite.setTextSize(14);
+            textViewAsetettuSahkopostiosoite.setTextColor(Color.RED);
+            textViewAsetettuSahkopostiosoite.setText("Sähköpostiosoitetta ei ole vielä asetettu!");
+        } else {
+            textViewAsetettuSahkopostiosoite.setText(oletussahkoposti);
+        }
+
+        // Asetetun sähköpostiosoitteen muokkaus (ei ainakaan vielä tarkista syötettyä sähköpostiosoitetta mitenkään)
+        imageButtonMuokkaaAsetettuaSahkopostiosoitetta = findViewById(R.id.ibMuokkaaAsetettuaSahkopostiosoitetta);
+        imageButtonMuokkaaAsetettuaSahkopostiosoitetta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
+                alert.setTitle("Aseta uusi oletussähköpostiosoite");
+
+                // EditText, johon uusi sähköpostiosoite kirjoitetaan
+                final EditText input = new EditText(v.getContext());
+                input.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS); // Olettaa syötteen olevan sähköpostiosoite
+                alert.setView(input);
+                // Toiminnallisuus, kun käyttäjä klikkaa "Vahvista"
+                alert.setPositiveButton("Vahvista", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Tallennetaan uusi osoite SharedPreferensseihin
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString("defaultEmail", input.getText().toString());
+                        editor.commit();
+                        // Asetetaan myös tvAsetettuSahkopostiosoite-textViewiin
+                        textViewAsetettuSahkopostiosoite.setText(input.getText().toString());
+                        textViewAsetettuSahkopostiosoite.setTextColor(Color.BLACK); // Varmistetaan, että tekstin väri vaihtuu
+                    }
+                });
+                // Toiminnallisuus, kun käyttäjä klikkaa "Peruuta"
+                alert.setNegativeButton("Peruuta", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Peru sähköpostiosoitteen muokkaus
+                    }
+                });
+                alert.show(); // Alert näkyviin
+            }
+        });
+
+
+
     }
 
     /*
